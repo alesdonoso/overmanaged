d87cb2c985131ef4be885f4e8581e35d8924c81b
alesdonoso <aledonosan@gmail.com>
2021-04-03 11:13:56 +0200

    added favicon and responsive form

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -4,7 +4,7 @@ module.exports = {
   author: `alesdonoso`,
   introduction: `A Product Management blog thought to underline things.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `static/favicon.svg`, // Add your favicon
   keywords: [`blog`, `product`, `management`],
   configs: {
     countOfInitialPost: 5, // Config your initial count of post
